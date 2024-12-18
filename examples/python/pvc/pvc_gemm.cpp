8c0182b28cf90391098f5f9325ee4eb3be435eb6
Chaikova <anastasiia.chaikova@intel.com>
2024-12-18 10:03:09 +0100

    Fix arguments

--- examples/python/pvc/pvc_gemm.cpp
+++ examples/python/pvc/pvc_gemm.cpp
@@ -4,21 +4,21 @@
 
 
 
-at::Tensor run_cutlass_gemm(const at::Tensor A,
-  const at::Tensor B,
-  const at::Tensor C,
+at::Tensor run_cutlass_gemm(const at::Tensor& A,
+  const at::Tensor& B,
+  const at::Tensor& C,
   float alpha, float beta, int iterations);
 
 // C++ interface
-at::Tensor call_cutlass_gemm(const at::Tensor A,
-  const at::Tensor B,
-  const at::Tensor C,
+at::Tensor call_cutlass_gemm(const at::Tensor& A,
+  const at::Tensor& B,
+  const at::Tensor& C,
   float alpha, float beta, int iterations) {
   return run_cutlass_gemm(A, B, C, alpha, beta, iterations);
 }
 
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
-  m.def("run", py::overload_cast<const at::Tensor, const at::Tensor, const at::Tensor, float, float, int>(&call_cutlass_gemm),
+  m.def("run", py::overload_cast<const at::Tensor&, const at::Tensor&, const at::Tensor&, float, float, int>(&call_cutlass_gemm),
         py::arg("A"), py::arg("B"), py::arg("C"),
         py::arg("alpha") = 1.f, py::arg("beta") = 0.f, py::arg("iterations") = 1);
 }
